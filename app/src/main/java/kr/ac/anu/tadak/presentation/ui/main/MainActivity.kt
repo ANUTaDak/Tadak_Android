eee2f0108a51bfd7bbac72f9debf108706c763b8
bhindor <20250199@student.anu.ac.kr>
2026-06-07 23:45:55 +0900

    ⚡️ :: mainView

--- app/src/main/java/kr/ac/anu/tadak/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/kr/ac/anu/tadak/presentation/ui/main/MainActivity.kt
@@ -2,17 +2,25 @@ package kr.ac.anu.tadak.presentation.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.ac.anu.tadak.R
+import kr.ac.anu.tadak.databinding.ActivityMainBinding
+import kr.ac.anu.tadak.presentation.viewmodel.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/kr/ac/anu/tadak/presentation/viewmodel/main/MainViewModel.kt
+++ app/src/main/java/kr/ac/anu/tadak/presentation/viewmodel/main/MainViewModel.kt
@@ -1,4 +1,6 @@
 package kr.ac.anu.tadak.presentation.viewmodel.main
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
 }
