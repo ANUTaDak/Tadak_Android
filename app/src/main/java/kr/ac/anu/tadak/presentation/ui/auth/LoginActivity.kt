f3dbe67d971e21b88ac9e257eead98b67313ca79
bhindor <20250199@student.anu.ac.kr>
2026-06-07 13:11:48 +0900

    ⚡️ :: add LoginView

--- app/src/main/java/kr/ac/anu/tadak/presentation/ui/auth/LoginActivity.kt
+++ app/src/main/java/kr/ac/anu/tadak/presentation/ui/auth/LoginActivity.kt
@@ -1,8 +1,12 @@
 package kr.ac.anu.tadak.presentation.ui.auth
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kr.ac.anu.tadak.R
@@ -23,7 +27,29 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
-        
+        viewSets()
 
+    }
+
+    private fun viewSets() {
+        val originText = binding.tvAppTitle.text.toString()
+        val spannable = SpannableStringBuilder(originText)
+
+        val targetWord = "TaDak"
+        val start = originText.indexOf(targetWord)
+        val end = start + targetWord.length
+
+        if (start != -1) {
+            val color = ContextCompat.getColor(this, R.color.tadak_blue)
+
+            spannable.setSpan(
+                ForegroundColorSpan(color),
+                start,
+                end,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+        binding.tvAppTitle.text = spannable
     }
 }
